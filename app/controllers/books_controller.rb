36712176001f2f8b9654d5d7886f4f81e1ddba32
EC2 Default User <ec2-user@ip-172-31-28-30.ec2.internal>
2022-03-09 11:45:09 +0000

    [Finish]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,20 @@
 class BooksController < ApplicationController
+  before_action :currect_user, only: [:edit, :update]
   def new
     @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to books_path
+    @books = Book.new(book_params)
+    @books.user_id = current_user.id
+
+    if @books.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@books.id)
     else
+      
+      @user = current_user
+      @book = Book.all
       render :index
     end
   end
@@ -51,8 +57,8 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  def correct_user
+
+  def currect_user
     @book = Book.find(params[:id])
     @user = @book.user
     redirect_to(books_path) unless @user == current_user
